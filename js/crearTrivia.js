d24957e76d6b9d5a65b9a9601c0060a1c6a48af5
DarthSeveros <joseivecas@gmail.com>
2023-02-17 11:27:44 -0300

    update added title tag added meta description added async script tag

--- js/crearTrivia.js
+++ js/crearTrivia.js
@@ -1,13 +1,18 @@
 function createTrivia() {
-    let nombre = document.getElementById("inputNombre");
-    let categoria = document.getElementById("inputCategoria");
+    let nombre = document.getElementById("inputNombre").value;
+    let categoria = document.getElementById("inputCategoria").value;
     let cantidadPreguntas = document.getElementById("inputCantidadPreguntas").value;
     return setInputPreguntas(nombre, categoria, cantidadPreguntas);
 }
 
 function setInputPreguntas(nombre, categoria, cantidadPreguntas) {
     let trivia = document.getElementById("trivia");
-    preguntas = ''
+    preguntas = `
+        <div>
+            <h2>${nombre}</h2>
+            <h4>${categoria}</h4>
+        </div>
+    `
     for(let i in Array.from({length: cantidadPreguntas}, (x, i) => i)){
         preguntas = preguntas.concat(`
         <div class="mb-3">
@@ -15,9 +20,35 @@ function setInputPreguntas(nombre, categoria, cantidadPreguntas) {
             <input type="text" class="form-control" id="inputPregunta${i}" aria-describedby="pregunta${i}Help">
             <div id="pregunta${i}Help" class="form-text">Ingresa la cantidad de preguntas</div>
         </div>
+        <div class="mb-3">
+            <div class="col-3">
+                <label class="form-label">A</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-3">
+                <label class="form-label">B</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-3">
+                <label class="form-label">C</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-3">
+                <label class="form-label">D</label>
+                <input type="text" class="form-control">
+            </div>
+        </div>
         `)
     }
+    preguntas = preguntas.concat(`
+        <div>
+            <a href="../index.html" class="btn btn-outline-mora">Volver</a>
+            <button id="send-trivia" class="btn btn-mora">Crear</button>
+        </div>
+    `)
     trivia.innerHTML = preguntas;
 }
 
-let trivia = document.getElementById("create-trivia").addEventListener("click", createTrivia);
+ document.getElementById("create-trivia").addEventListener("click", createTrivia);
+ 
+
